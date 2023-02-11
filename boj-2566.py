1c1b0ebe4451f61694bea7e54c9399280b241dc2
subin <subin21cc@gmail.com>
2023-02-12 01:22:58 +0900

    BOJ 1문제 학습 (2566)

--- boj-2566.py
+++ boj-2566.py
@@ -1,8 +1,21 @@
 # https://www.acmicpc.net/problem/2566
+import sys
+input = sys.stdin.readline
 matrix = []
 
-for i in range(9):
-    row = int(input().split())
-    matrix.append(row)
+for _ in range(9):
+    matrix.append(list(map(int, input().split())))
 
-max = []
+x = 0
+y = 0
+max = -1
+
+for i in range(9):
+    for j in range(9):
+        if matrix[i] [j] > max:
+            max = matrix[i] [j]
+            x = i + 1
+            y = j + 1
+
+print(max)
+print(x, y)
