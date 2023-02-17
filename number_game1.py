f00fef9107adb58369130f181310063c5fcf1fda
subin <subin21cc@gmail.com>
2023-02-17 23:19:17 +0900

    숫자맞추기1 학습 완료, 숫자맞추기2 학습 중

--- number_game1.py
+++ number_game1.py
@@ -1,7 +1,7 @@
 # 숫자 맞추기 게임 1
 '''
-컴퓨터가 1~n 중 숫자 하나를 랜덤으로 고름
-(n은 사용자로부터 입력받음)
+컴퓨터가 1~max_num 중 숫자 하나를 랜덤으로 고름
+(max_num은 사용자로부터 입력받음)
 사용자가 숫자 하나를 입력
 입력받은 숫자가 컴퓨터가 고른 숫자 보다 높으면(낮으면) '맞춰야할 수보다 높습니다(낮습니다)'를 출력
 사용자가 숫자를 맞출 때끼지 반복
@@ -11,12 +11,16 @@
 import random
 max_num = 100
 ran = random.randrange(1, max_num+1)
-guess = 0
+guess = ''
 
-while guess == ran:
+while guess != ran:
     guess = int(input())
     if guess > ran:
-        print('')
+        print(guess, '보다 작습니다.')
+    elif guess < ran:
+        print(guess, '보다 큽니다.')
+    else:
+        print('맞았습니다.')
 
 
 
